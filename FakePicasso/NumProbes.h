987b37341ca18fe70d01792c887eb1c0b722a719
g.bykova <g.bykova@g.nsu.ru>
2023-09-29 14:41:03 +0700

    fake

---
+++ FakePicasso/NumProbes.h
@@ -0,0 +1,9 @@
+#pragma once
+///////////////////////////////// онстанты дл€ зондов////////////////////////////////////
+//зонды ¬» ѕЅ
+#define LWD_LOOCH_VIKPB_ROL                1039 //частота 880.555 к√ц
+#define LWD_LOOCH_VIKPB_ROLD			   1040 //частота 880.555 к√ц
+#define LWD_LOOCH_VIKPB_ROLE			   1041 //частота 880.555 к√ц
+#define LWD_LOOCH_VIKPB_ROH				   1042 //частота 3.50555 ћ√ц
+#define LWD_LOOCH_VIKPB_ROHD			   1043 //частота 3.50555 ћ√ц
+#define LWD_LOOCH_VIKPB_ROHE			   1044 //частота 3.50555 ћ√ц
